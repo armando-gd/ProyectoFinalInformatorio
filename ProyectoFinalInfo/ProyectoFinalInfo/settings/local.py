58795c80fc4b052d184d582aa76d9df6df226309
Mauricio Alejandro Pérez <map.j91@gmail.com>
2022-08-03 18:36:13 -0300

    Initial commit

---
+++ ProyectoFinalInfo/ProyectoFinalInfo/settings/local.py
@@ -0,0 +1,12 @@
+from base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sitioInfo',
+        'USER': 'Test'
+    }
+}
---
+++ ProyectoFinalInfo/ProyectoFinalInfo/settings/production.py
@@ -0,0 +1,16 @@
+from base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
